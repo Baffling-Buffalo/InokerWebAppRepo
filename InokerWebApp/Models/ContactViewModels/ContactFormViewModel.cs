fd0af5c9c59dde58503e45e47c94007293ff6e7b
Nemanja Miric <nemanjabr1@gmail.com>
2018-10-25 16:47:08 +0200

    contact

---
+++ InokerWebApp/Models/ContactViewModels/ContactFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InokerWebApp.Models.ContactViewModels
+{
+    public class ContactFormViewModel
+    {
+        [Required]
+        [Display(Name = "Ime")]
+        public string Name { get; set; }
+        [Display(Name = "Prezime")]
+        public string Prezime { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Poruka")]
+        public string Message { get; set; }
+    }
+}
