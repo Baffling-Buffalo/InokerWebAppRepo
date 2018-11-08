9d7ad5d38d349e66dceaadc89759b774b65ea940
DESKTOP-Q4H68L3\neman <nemanjabr1@gmail.com>
2018-11-08 16:53:54 +0100

    a

--- InokerWebApp/Models/InokerwebappdbContext.cs
+++ InokerWebApp/Models/InokerwebappdbContext.cs
@@ -22,11 +22,11 @@ namespace InokerWebApp.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=InokerWebAppDb;Integrated Security=True");
-            }
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=InokerWebAppDb;Integrated Security=True");
+//            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
